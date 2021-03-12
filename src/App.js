9360f27045e868f420a95ffd023508489b48a194
imhilla <hillaryodhiambo@gmail.com>
2021-03-12 11:27:39 +0300

    add left sidebar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
-
 function App() {
   return (
     <div className="app">
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,10 +6,23 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__top">
-        <img src="" alt="" />
-        <Avatar />
+        <img src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="" />
+        <Avatar className="sidebar__avatar" />
         <h2>Hillary Kiptoo</h2>
-        <h4>hillaryodhiambo282@gmail.com</h4>
+        <h4>Full stack developer</h4>
+      </div>
+      <div className="sidebar__stats">
+        <div className="sidebar_stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,543</p>
+        </div>
+        <div className="sidebar_stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">2,448</p>
+        </div>
+      </div>
+      <div className="sidebar__bottom">
+        <p>Recent</p>
       </div>
     </div>
   )
