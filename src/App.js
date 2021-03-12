2a3e792b6776520cd0a1a4d3c67918761c71ebf0
imhilla <hillaryodhiambo@gmail.com>
2021-03-12 16:34:12 +0300

    add login screen

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
+import { selectUser } from './features/userSlice';
 import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
 function App() {
+  const user = useSelector(selectUser)
   return (
     <div className="app">
       <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-      </div>
-      {/* widgets */}
+      {!user ? <Login /> : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          {/* widgets */}
+        </div>
+      )}
     </div>
   );
 }
